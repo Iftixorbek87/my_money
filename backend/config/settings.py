4079d9764c820c37d2d863f25867b14b947e71a5
Iftixorbek87 <odiloviftixorbek@email.com>
2026-02-27 23:40:47 +0500

    Complete Render deployment setup: optimized settings and Procfile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,17 +64,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# Heroku PostgreSQL
 import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-    )
-}
 
-# PostgreSQL for Production (if DATABASE_URL is set)
+# Production database (Render PostgreSQL)
 if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    # Development database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
